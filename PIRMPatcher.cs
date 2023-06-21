25402063d0c256b3bd9284aa4f70dce8b513b6d6
dvize <dvize404@gmail.com>
2023-06-21 12:10:17 -0700

    Update for 3.5.7

--- PIRMPatcher.cs
+++ PIRMPatcher.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Reflection;
 using Aki.Reflection.Patching;
 using EFT.InventoryLogic;
 using EFT.UI;
 using EFT.UI.DragAndDrop;
-using JetBrains.Annotations;
+using HarmonyLib;
 
 namespace PIRM
 {
     public class PIRMMethod17Patch : ModulePatch
     {
-        protected override MethodBase GetTargetMethod() => typeof(ItemSpecificationPanel).GetMethod("method_17", BindingFlags.NonPublic | BindingFlags.Instance);
+        protected override MethodBase GetTargetMethod() => AccessTools.Method(typeof(ItemSpecificationPanel), "method_17");
 
         [PatchPrefix]
         public static bool PatchPrefix(ref KeyValuePair<EModLockedState, string> __result, Slot slot)
         {
-            string text = (slot.ContainedItem != null) ? slot.ContainedItem.Name.Localized(null) : string.Empty;
+            string text = ((slot.ContainedItem != null) ? slot.ContainedItem.Name.Localized(null) : string.Empty);
             __result = new KeyValuePair<EModLockedState, string>(EModLockedState.Unlocked, text);
 
             return false;
@@ -26,18 +25,17 @@ namespace PIRM
 
 
     // THIS METHOD IS Causing an issue of not being able to move items back at all probably.... or maybe need new one for itemspecifcationpanel
-    public class PIRMGlass2426Smethod1 : ModulePatch
+    public class PIRMGlass2428Smethod1 : ModulePatch
     {
-        protected override MethodBase GetTargetMethod() => typeof(GClass2429).GetMethod("smethod_1", BindingFlags.NonPublic | BindingFlags.Static);
+        protected override MethodBase GetTargetMethod() => AccessTools.Method(typeof(GClass2428), "smethod_1");
 
         [PatchPrefix]
-        [Obsolete]
-        public static bool Prefix(Item item, ItemAddress to, TraderControllerClass itemController, ref GStruct326<GClass2897> __result)
+        public static bool Prefix(Item item, ItemAddress to, TraderControllerClass itemController, ref GStruct323<GClass2898> __result)
         {
 
-            if (GClass1757.InRaid)
+            if (GClass1756.InRaid)
             {
-                __result = GClass2897._;
+                __result = GClass2898._;
                 return false;
             }
 
@@ -47,10 +45,10 @@ namespace PIRM
 
     public class SlotViewMethod2 : ModulePatch
     {
-        protected override MethodBase GetTargetMethod() => typeof(EFT.InventoryLogic.Item).GetMethod("CheckAction", BindingFlags.Public | BindingFlags.Instance);
+        protected override MethodBase GetTargetMethod() => AccessTools.Method(typeof(EFT.InventoryLogic.Item), "CheckAction");
 
         [PatchPrefix]
-        static bool Prefix(Item __instance, [CanBeNull] ItemAddress location, ref bool __result)
+        public static bool Prefix(ItemAddress location, ref bool __result)
         {
             // Set the result to true and return false to skip the original method
             __result = true;
@@ -63,10 +61,10 @@ namespace PIRM
     //need this one
     public class EFTInventoryLogicModPatch : ModulePatch
     {
-        protected override MethodBase GetTargetMethod() => typeof(EFT.InventoryLogic.Mod).GetMethod("CanBeMoved", BindingFlags.Public | BindingFlags.Instance);
+        protected override MethodBase GetTargetMethod() => AccessTools.Method(typeof(EFT.InventoryLogic.Mod), "CanBeMoved");
 
         [PatchPrefix]
-        static bool Prefix(IContainer toContainer, ref GStruct326<bool> __result)
+        public static bool Prefix(IContainer toContainer, ref GStruct323<bool> __result)
         {
             __result = true;
             return false;
@@ -74,40 +72,18 @@ namespace PIRM
 
     }
 
-    /*    public class PIRMGlass2426Smethod2 : ModulePatch
-        {
-            protected override MethodBase GetTargetMethod()
-            {
-                return typeof(GClass2426).GetMethod("smethod_2", BindingFlags.NonPublic | BindingFlags.Static);
-            }
-
-            [PatchPrefix]
-            static bool Prefix(ItemAddress to, ref IContainer __result)
-            {
-                __result = null;
-                return false;
-            }
-        }*/
-
-    /*public class Glass2426CheckMissingParts: ModulePatch
-    {
-        protected override MethodBase GetTargetMethod()
-        {
-            return typeof(GClass2426).GetMethod("CheckMissingParts", BindingFlags.Public | BindingFlags.Static);
-        }
-
-        [PatchPrefix]
-        static bool Prefix(LootItemClass compoundItem, ItemAddress to, TraderControllerClass itemController, out Slot.GClass2863 error)
-        {
-            error = null;
-            return true;
-        }
-    }*/
-
-
-
-
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
 
--- PIRMPlugin.cs
+++ PIRMPlugin.cs
@@ -1,27 +1,38 @@
-﻿using BepInEx;
+﻿using System;
+using System.Diagnostics;
+using BepInEx;
+using VersionChecker;
 
 namespace PIRM
 {
 
-    [BepInPlugin("com.dvize.PIRM", "dvize.PIRM", "1.5.2")]
+    [BepInPlugin("com.dvize.PIRM", "dvize.PIRM", "1.5.5")]
     class PIRMPlugin : BaseUnityPlugin
     {
+        private void Awake()
+        {
+            CheckEftVersion();
+        }
         private void Start()
         {
             new PIRMMethod17Patch().Enable();
-            new PIRMGlass2426Smethod1().Enable();
+            new PIRMGlass2428Smethod1().Enable();
             new SlotViewMethod2().Enable();
             new EFTInventoryLogicModPatch().Enable();
 
-
-            //Extras?  Check again later
-            //new PIRMGlass2426Smethod2().Enable();
-            //new Glass2426CheckMissingParts().Enable();
-            //new SetRaidModdablePatch().Enable();
-            //new ModSlotViewPatch().Enable();
-
-
         }
 
+        private void CheckEftVersion()
+        {
+            // Make sure the version of EFT being run is the correct version
+            int currentVersion = FileVersionInfo.GetVersionInfo(BepInEx.Paths.ExecutablePath).FilePrivatePart;
+            int buildVersion = TarkovVersion.BuildVersion;
+            if (currentVersion != buildVersion)
+            {
+                Logger.LogError($"ERROR: This version of {Info.Metadata.Name} v{Info.Metadata.Version} was built for Tarkov {buildVersion}, but you are running {currentVersion}. Please download the correct plugin version.");
+                EFT.UI.ConsoleScreen.LogError($"ERROR: This version of {Info.Metadata.Name} v{Info.Metadata.Version} was built for Tarkov {buildVersion}, but you are running {currentVersion}. Please download the correct plugin version.");
+                throw new Exception($"Invalid EFT Version ({currentVersion} != {buildVersion})");
+            }
+        }
     }
 }
